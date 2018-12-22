c136e24cbe7f184e6b987c707fbabe32d39d16ec
Felix Razikov <frazykov@gmail.com>
2018-12-22 22:00:23 +0200

    Q1

--- hw3/hw3q1.c
+++ hw3/hw3q1.c
@@ -8,35 +8,38 @@
 4. If there is 'B'*3 or more, the player loses $20
 
 */
-
-int currChar = 0;
-int prevChar = 0;
-int prevPrevChar = 0;
-int strikeCount = 0;
-int negativeCount = 0;
-int blackCount = 0;
-
-int summary;
-
-while (currChar = getchar() != '!')
+int main()
 {
-  /* Check if 'SUP' is supplied */
-  if (prevPrevChar == 'S' && prevChar == 'U' && currChar == 'P')
-    strikeCount++;
+  int currChar = 0;
+  int prevChar = 0;
+  int prevPrevChar = 0;
+  int strikeCount = 0;
+  int negativeCount = 0;
+  int blackCount = 0;
 
-  if (currChar == 'N')
-    negativeCount++;
-  
-  if (currChar == 'B')
-    blackCount++;
+  int summary;
 
-  /* Move chars backwards */
-  prevPrevChar = prevChar;
-  prevChar = currChar;  
+  while ((currChar = getchar()) != '!')
+  {
+    /* Check if 'SUP' is supplied */
+    if (prevPrevChar == 'S' && prevChar == 'U' && currChar == 'P')
+      strikeCount++;
+
+    if (currChar == 'N')
+      negativeCount++;
+
+    if (currChar == 'B')
+      blackCount++;
+
+    /* Move chars backwards */
+    prevPrevChar = prevChar;
+    prevChar = currChar;
+  }
+
+  summary = (strikeCount * 100) - (negativeCount * 10) - ((blackCount >= 3) ? 20 : 0);
+
+  if (summary >= 0)
+    printf("You gain %d", summary);
+  else
+    printf("You lost %d", -summary);
 }
-
-summary = (strikeCount * 100) - (negativeCount * 10) - ((blackCount >= 3) ? 20 : 0);
-
-if (summary >= 0)
-  printf("You gain %d", summary)
-else printf("You lost %d", -summary)
