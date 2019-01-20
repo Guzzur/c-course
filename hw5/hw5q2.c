cde6375969d304140ffb349a321f228ee01d95da
Felix Razikov <frazykov@gmail.com>
2019-01-20 21:13:05 +0200

    hw5q2 first single input parsing completed

--- hw5/hw5q2.c
+++ hw5/hw5q2.c
@@ -2,6 +2,10 @@
 #define OK 0
 #define FAIL 1
 #define MAX_LENGTH 50
+#define SEPARATOR ','
+#define STR_LAST_INPUT "EZPZ"
+#define ADD_LAST_INPUT "1337"
+#define ASCII_NUM_OFFSET 48
 
 int str_len(char str[])
 {
@@ -17,8 +21,9 @@ void print_str_add(char str[], char add[])
   int i;
   for (i = 0; i < str_len(str) && i < str_len(add); i++)
   {
-    putchar(str[i] + add[i]);
+    putchar(str[i] + add[i] - ASCII_NUM_OFFSET);
   }
+  putchar('\n');
 }
 
 int str_cmp(char str_1[], char str_2[])
@@ -35,12 +40,32 @@ int str_cmp(char str_1[], char str_2[])
   return c1 - c2;
 }
 
+void split_str(char orig[], char str_1[MAX_LENGTH], char str_2[MAX_LENGTH], char separator)
+{
+  int i, j;
+  for (i = 0; orig[i] != separator; i++)
+    str_1[i] = orig[i];
+  str_1[i] = 0;
+
+  j = i + 1;
+  for (i = 0; orig[j + i]; i++)
+    str_2[i] = orig[j + i];
+  str_2[i] = 0;
+}
+
 int main()
 {
   char currChar;
+  char originalStr[MAX_LENGTH * 2 + 1];
   char str[MAX_LENGTH], add[MAX_LENGTH];
 
-  scanf("%s,%s", &str, &add);
+  scanf("%s", originalStr);
+
+  split_str(originalStr, &str, &add, SEPARATOR);
+
+  printf("%s\n", originalStr);
+  printf("%s\n", str);
+  printf("%s\n", add);
 
   print_str_add(str, add);
 
